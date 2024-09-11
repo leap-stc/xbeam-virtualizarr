dc4a85ee0cd6a2bc45b8e1f35e1e24468f4083fb
Raphael Hagen <norlandrhagen@gmail.com>
2024-09-10 19:49:52 -0600

    wip

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="dataflow_job",
+    name="xbeam_virtualizarr",
     version="0.0.1",
     install_requires=[
         # 'apache-beam[gcp]',
